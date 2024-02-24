b165dccd84fbaa9da1f3ddd0200b579fcbd00990
gegko <ruqx01@gmail.com>
2024-02-25 01:59:38 +0300

    updated backup file

--- backup.sql
+++ backup.sql
@@ -108,92 +108,80 @@ ALTER TABLE public.question OWNER TO absolutpos;
 -- Data for Name: answer; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.answer (text, business_id, question_id, id, created_at) FROM stdin;
-\.
 
 
 --
 -- Data for Name: business; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.business (name, city, address, id, created_at, businesstype_id, businessarea_id) FROM stdin;
-moi novii bizik	piter	sovetskaya 1	96d80800-1885-4e51-87bd-c003064a08ef	2024-02-24 21:19:04.045728	9d63ba8a-3e92-4784-bdfa-36c128042212	b5bde420-1fbe-44a4-a7ff-98e5681d7828
-moi osnovnoi bizik	moscow	arbat 12	b975e661-4edf-4d60-be77-6fa02c967a87	2024-02-24 21:13:28.44597	0b7acf2d-e93c-43ac-882e-a3994d4c943d	fdf828c0-ca26-45a7-b611-f9fd7a18daaf
-\.
+INSERT INTO public.business (name, city, address, id, created_at, businesstype_id, businessarea_id) VALUES ('moi novii bizik', 'piter', 'sovetskaya 1', '96d80800-1885-4e51-87bd-c003064a08ef', '2024-02-24 21:19:04.045728', '9d63ba8a-3e92-4784-bdfa-36c128042212', 'b5bde420-1fbe-44a4-a7ff-98e5681d7828');
+INSERT INTO public.business (name, city, address, id, created_at, businesstype_id, businessarea_id) VALUES ('moi osnovnoi bizik', 'moscow', 'arbat 12', 'b975e661-4edf-4d60-be77-6fa02c967a87', '2024-02-24 21:13:28.44597', '0b7acf2d-e93c-43ac-882e-a3994d4c943d', 'fdf828c0-ca26-45a7-b611-f9fd7a18daaf');
 
 
 --
 -- Data for Name: businessarea; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.businessarea (name, businesstype_id, id, created_at) FROM stdin;
-Ресторан	9d63ba8a-3e92-4784-bdfa-36c128042212	f3748f92-1b68-478a-a529-af5384ed2de3	2024-02-23 23:43:47.399969
-Бар	9d63ba8a-3e92-4784-bdfa-36c128042212	b5bde420-1fbe-44a4-a7ff-98e5681d7828	2024-02-23 23:43:50.042779
-Кафе	9d63ba8a-3e92-4784-bdfa-36c128042212	70e5357e-92b6-4a33-853b-68d088ec0c0f	2024-02-23 23:43:54.380263
-Магазин	0b7acf2d-e93c-43ac-882e-a3994d4c943d	26373ba9-14f6-4a8d-8084-008b98818bb0	2024-02-23 23:44:00.113752
-Киоск	0b7acf2d-e93c-43ac-882e-a3994d4c943d	fdf828c0-ca26-45a7-b611-f9fd7a18daaf	2024-02-23 23:44:05.199754
-DRAFT	0870f0bc-0976-49ce-a81c-62e586d006ab	7e599f9f-e004-4ab5-b06b-120b8c9e738b	2024-02-23 23:50:07.447741
-\.
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('Ресторан', '9d63ba8a-3e92-4784-bdfa-36c128042212', 'f3748f92-1b68-478a-a529-af5384ed2de3', '2024-02-23 23:43:47.399969');
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('Бар', '9d63ba8a-3e92-4784-bdfa-36c128042212', 'b5bde420-1fbe-44a4-a7ff-98e5681d7828', '2024-02-23 23:43:50.042779');
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('Кафе', '9d63ba8a-3e92-4784-bdfa-36c128042212', '70e5357e-92b6-4a33-853b-68d088ec0c0f', '2024-02-23 23:43:54.380263');
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('Магазин', '0b7acf2d-e93c-43ac-882e-a3994d4c943d', '26373ba9-14f6-4a8d-8084-008b98818bb0', '2024-02-23 23:44:00.113752');
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('Киоск', '0b7acf2d-e93c-43ac-882e-a3994d4c943d', 'fdf828c0-ca26-45a7-b611-f9fd7a18daaf', '2024-02-23 23:44:05.199754');
+INSERT INTO public.businessarea (name, businesstype_id, id, created_at) VALUES ('DRAFT', '0870f0bc-0976-49ce-a81c-62e586d006ab', '7e599f9f-e004-4ab5-b06b-120b8c9e738b', '2024-02-23 23:50:07.447741');
 
 
 --
 -- Data for Name: businessareaquestionlink; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.businessareaquestionlink (businessarea_id, question_id) FROM stdin;
-f3748f92-1b68-478a-a529-af5384ed2de3	c874ae9d-a403-4a0e-b782-9b6ce582765d
-b5bde420-1fbe-44a4-a7ff-98e5681d7828	c874ae9d-a403-4a0e-b782-9b6ce582765d
-70e5357e-92b6-4a33-853b-68d088ec0c0f	c874ae9d-a403-4a0e-b782-9b6ce582765d
-f3748f92-1b68-478a-a529-af5384ed2de3	3c076b8a-cb9b-4365-8042-caae5b19dd78
-f3748f92-1b68-478a-a529-af5384ed2de3	477bc67e-83af-4dfc-9e38-97dc8fd9a8b1
-70e5357e-92b6-4a33-853b-68d088ec0c0f	477bc67e-83af-4dfc-9e38-97dc8fd9a8b1
-7e599f9f-e004-4ab5-b06b-120b8c9e738b	d9dfb614-b11e-49b1-8ad3-4cfec0fea65b
-7e599f9f-e004-4ab5-b06b-120b8c9e738b	42e49a17-0c8e-454c-8355-274ce6c95ed0
-26373ba9-14f6-4a8d-8084-008b98818bb0	10197439-e1d7-4c2b-9817-1f3ed43434c5
-f3748f92-1b68-478a-a529-af5384ed2de3	10197439-e1d7-4c2b-9817-1f3ed43434c5
-b5bde420-1fbe-44a4-a7ff-98e5681d7828	10197439-e1d7-4c2b-9817-1f3ed43434c5
-70e5357e-92b6-4a33-853b-68d088ec0c0f	10197439-e1d7-4c2b-9817-1f3ed43434c5
-fdf828c0-ca26-45a7-b611-f9fd7a18daaf	10197439-e1d7-4c2b-9817-1f3ed43434c5
-fdf828c0-ca26-45a7-b611-f9fd7a18daaf	d39e9e03-496e-4d9b-bfa8-41a12e4d2721
-b5bde420-1fbe-44a4-a7ff-98e5681d7828	d39e9e03-496e-4d9b-bfa8-41a12e4d2721
-26373ba9-14f6-4a8d-8084-008b98818bb0	a70ae79c-b129-4420-8625-c8115b62ca43
-7e599f9f-e004-4ab5-b06b-120b8c9e738b	b30c51ea-7675-4d28-aaae-f559655c5fda
-7e599f9f-e004-4ab5-b06b-120b8c9e738b	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-f3748f92-1b68-478a-a529-af5384ed2de3	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-b5bde420-1fbe-44a4-a7ff-98e5681d7828	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-70e5357e-92b6-4a33-853b-68d088ec0c0f	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-26373ba9-14f6-4a8d-8084-008b98818bb0	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-fdf828c0-ca26-45a7-b611-f9fd7a18daaf	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b
-\.
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('f3748f92-1b68-478a-a529-af5384ed2de3', 'c874ae9d-a403-4a0e-b782-9b6ce582765d');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('b5bde420-1fbe-44a4-a7ff-98e5681d7828', 'c874ae9d-a403-4a0e-b782-9b6ce582765d');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('70e5357e-92b6-4a33-853b-68d088ec0c0f', 'c874ae9d-a403-4a0e-b782-9b6ce582765d');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('f3748f92-1b68-478a-a529-af5384ed2de3', '3c076b8a-cb9b-4365-8042-caae5b19dd78');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('f3748f92-1b68-478a-a529-af5384ed2de3', '477bc67e-83af-4dfc-9e38-97dc8fd9a8b1');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('70e5357e-92b6-4a33-853b-68d088ec0c0f', '477bc67e-83af-4dfc-9e38-97dc8fd9a8b1');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('7e599f9f-e004-4ab5-b06b-120b8c9e738b', 'd9dfb614-b11e-49b1-8ad3-4cfec0fea65b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('7e599f9f-e004-4ab5-b06b-120b8c9e738b', '42e49a17-0c8e-454c-8355-274ce6c95ed0');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('26373ba9-14f6-4a8d-8084-008b98818bb0', '10197439-e1d7-4c2b-9817-1f3ed43434c5');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('f3748f92-1b68-478a-a529-af5384ed2de3', '10197439-e1d7-4c2b-9817-1f3ed43434c5');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('b5bde420-1fbe-44a4-a7ff-98e5681d7828', '10197439-e1d7-4c2b-9817-1f3ed43434c5');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('70e5357e-92b6-4a33-853b-68d088ec0c0f', '10197439-e1d7-4c2b-9817-1f3ed43434c5');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('fdf828c0-ca26-45a7-b611-f9fd7a18daaf', '10197439-e1d7-4c2b-9817-1f3ed43434c5');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('fdf828c0-ca26-45a7-b611-f9fd7a18daaf', 'd39e9e03-496e-4d9b-bfa8-41a12e4d2721');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('b5bde420-1fbe-44a4-a7ff-98e5681d7828', 'd39e9e03-496e-4d9b-bfa8-41a12e4d2721');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('26373ba9-14f6-4a8d-8084-008b98818bb0', 'a70ae79c-b129-4420-8625-c8115b62ca43');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('7e599f9f-e004-4ab5-b06b-120b8c9e738b', 'b30c51ea-7675-4d28-aaae-f559655c5fda');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('7e599f9f-e004-4ab5-b06b-120b8c9e738b', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('f3748f92-1b68-478a-a529-af5384ed2de3', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('b5bde420-1fbe-44a4-a7ff-98e5681d7828', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('70e5357e-92b6-4a33-853b-68d088ec0c0f', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('26373ba9-14f6-4a8d-8084-008b98818bb0', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
+INSERT INTO public.businessareaquestionlink (businessarea_id, question_id) VALUES ('fdf828c0-ca26-45a7-b611-f9fd7a18daaf', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b');
 
 
 --
 -- Data for Name: businesstype; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.businesstype (name, id, created_at) FROM stdin;
-Ритейл	0b7acf2d-e93c-43ac-882e-a3994d4c943d	2024-02-23 23:43:33.324024
-Общепит	9d63ba8a-3e92-4784-bdfa-36c128042212	2024-02-23 23:43:37.621785
-DRAFT	0870f0bc-0976-49ce-a81c-62e586d006ab	2024-02-23 23:50:01.38838
-\.
+INSERT INTO public.businesstype (name, id, created_at) VALUES ('Ритейл', '0b7acf2d-e93c-43ac-882e-a3994d4c943d', '2024-02-23 23:43:33.324024');
+INSERT INTO public.businesstype (name, id, created_at) VALUES ('Общепит', '9d63ba8a-3e92-4784-bdfa-36c128042212', '2024-02-23 23:43:37.621785');
+INSERT INTO public.businesstype (name, id, created_at) VALUES ('DRAFT', '0870f0bc-0976-49ce-a81c-62e586d006ab', '2024-02-23 23:50:01.38838');
 
 
 --
 -- Data for Name: question; Type: TABLE DATA; Schema: public; Owner: absolutpos
 --
 
-COPY public.question (text, id, created_at) FROM stdin;
-Самый магазинный вопрос	a70ae79c-b129-4420-8625-c8115b62ca43	2024-02-24 17:57:06.555672
-Ещё один вопрос для всех	1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b	2024-02-24 18:00:45.937823
-Вопрос для всех	10197439-e1d7-4c2b-9817-1f3ed43434c5	2024-02-24 17:55:29.520167
-Черновик вопроса для ресторана	d9dfb614-b11e-49b1-8ad3-4cfec0fea65b	2024-02-23 23:50:19.710016
-Вопрос для ресторана	3c076b8a-cb9b-4365-8042-caae5b19dd78	2024-02-23 23:44:50.937384
-Вопрос для общепита	c874ae9d-a403-4a0e-b782-9b6ce582765d	2024-02-23 23:44:28.342512
-Ресторан или кафе?	477bc67e-83af-4dfc-9e38-97dc8fd9a8b1	2024-02-23 23:45:04.354479
-Старый вопрос для киосков	42e49a17-0c8e-454c-8355-274ce6c95ed0	2024-02-23 23:50:43.342719
-Вопрос, который не подошёл ритейлу и общепиту	b30c51ea-7675-4d28-aaae-f559655c5fda	2024-02-24 17:58:06.303588
-Назовите 3 цифры на обороте карты	d39e9e03-496e-4d9b-bfa8-41a12e4d2721	2024-02-24 17:56:04.589821
-\.
+INSERT INTO public.question (text, id, created_at) VALUES ('Самый магазинный вопрос', 'a70ae79c-b129-4420-8625-c8115b62ca43', '2024-02-24 17:57:06.555672');
+INSERT INTO public.question (text, id, created_at) VALUES ('Ещё один вопрос для всех', '1e3f7f5c-fab1-46cd-be8b-b93a3265ca9b', '2024-02-24 18:00:45.937823');
+INSERT INTO public.question (text, id, created_at) VALUES ('Вопрос для всех', '10197439-e1d7-4c2b-9817-1f3ed43434c5', '2024-02-24 17:55:29.520167');
+INSERT INTO public.question (text, id, created_at) VALUES ('Черновик вопроса для ресторана', 'd9dfb614-b11e-49b1-8ad3-4cfec0fea65b', '2024-02-23 23:50:19.710016');
+INSERT INTO public.question (text, id, created_at) VALUES ('Вопрос для ресторана', '3c076b8a-cb9b-4365-8042-caae5b19dd78', '2024-02-23 23:44:50.937384');
+INSERT INTO public.question (text, id, created_at) VALUES ('Вопрос для общепита', 'c874ae9d-a403-4a0e-b782-9b6ce582765d', '2024-02-23 23:44:28.342512');
+INSERT INTO public.question (text, id, created_at) VALUES ('Ресторан или кафе?', '477bc67e-83af-4dfc-9e38-97dc8fd9a8b1', '2024-02-23 23:45:04.354479');
+INSERT INTO public.question (text, id, created_at) VALUES ('Старый вопрос для киосков', '42e49a17-0c8e-454c-8355-274ce6c95ed0', '2024-02-23 23:50:43.342719');
+INSERT INTO public.question (text, id, created_at) VALUES ('Вопрос, который не подошёл ритейлу и общепиту', 'b30c51ea-7675-4d28-aaae-f559655c5fda', '2024-02-24 17:58:06.303588');
+INSERT INTO public.question (text, id, created_at) VALUES ('Назовите 3 цифры на обороте карты', 'd39e9e03-496e-4d9b-bfa8-41a12e4d2721', '2024-02-24 17:56:04.589821');
 
 
 --
